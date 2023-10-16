ed80d88c25b3ea88e3a41ba0b683c6b7b0308e52
michal pitr <michal.pitr.altis@gmail.com>
2023-10-16 22:19:42 +0200

    initial commit

---
+++ ccwc.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+type Flags struct {
+	bytes bool
+	lines bool
+	words bool
+	chars bool
+}
+
+func parseArguments(flags *Flags, fileName *string) {
+	args := os.Args[1:]
+	fileCount := 0
+
+	for _, arg := range args {
+		if strings.HasPrefix(arg, "-") && len(arg) > 1 {
+			for _, runeValue := range arg[1:] {
+				switch runeValue {
+				case 'c':
+					flags.bytes = true
+				case 'w':
+					flags.words = true
+				case 'l':
+					flags.lines = true
+				case 'm':
+					flags.chars = true
+				default:
+					fmt.Printf("Unknown flag: %c\n", runeValue)
+				}
+			}
+		} else {
+			fileCount++
+			if fileCount > 1 {
+				os.Stderr.WriteString("More than one filename provided.")
+				os.Exit(2)
+			}
+			*fileName = arg
+		}
+	}
+}
+
+func readData(fileName *string) []byte {
+	var dat []byte
+	var err error
+	if *fileName == "" {
+		// Read input from stdin until EOF or ctrl+D is hit.
+		var buf bytes.Buffer
+		_, err := io.Copy(&buf, os.Stdin)
+		if err != nil {
+			os.Stderr.WriteString("Error reading from stdin")
+			os.Exit(1)
+		}
+		dat = buf.Bytes()
+	} else {
+		dat, err = os.ReadFile(*fileName)
+		if err != nil {
+			os.Stderr.WriteString("Failed to read file.")
+			os.Exit(1)
+		}
+	}
+	return dat
+}
+
+func main() {
+	var flags Flags
+	var counts [4]int
+	var fileName string
+
+	parseArguments(&flags, &fileName)
+	dat := readData(&fileName)
+
+	// If no flags are set, default to bytes, words, and lines.
+	if !flags.bytes && !flags.words && !flags.lines && !flags.chars {
+		flags.bytes, flags.words, flags.lines = true, true, true
+	}
+
+	if flags.lines {
+		lineCount := 0
+		for _, b := range dat {
+			if b == '\n' {
+				lineCount++
+			}
+		}
+		counts[0] = lineCount
+	}
+
+	if flags.words {
+		counts[1] = len(strings.Fields(string(dat)))
+	}
+
+	if flags.chars {
+		charCount := 0
+		for range string(dat) {
+			charCount++
+		}
+		counts[2] = charCount
+	}
+
+	if flags.bytes {
+		counts[3] = len(dat)
+	}
+
+	// Output results to stdout.
+	for _, value := range counts {
+		fmt.Printf("  %d", value)
+	}
+
+	// File name is "" when used with pipe.
+	fmt.Printf("  %s\n", fileName)
+}
